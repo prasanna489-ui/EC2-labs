741cf251edd12c3ff2d57579ce22182a35ca74b4
prasanna489-ui <83752553+prasanna489-ui@users.noreply.github.com>
2021-05-23 19:40:59 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -14,18 +14,3 @@ resource "aws_instance" "prasanna-ec2" {
 	Name = "prasanna-ansible"
   }
 
-provisioner "remote-exec" {
-  inline = ["echo 'build ssh connection' >> /tmp/file "]
-  connection {
-	host = self.public_ip
-	type = "ssh"
-	user = "ubuntu"
-	private_key = file("./prasanna-key")
-
- }
-}
-provisioner "local-exec" {
-	command = "ansible-playbook -i ${aws_instance.prasanna-ec2.public_ip}, --private-key ${var.PATH_TO_PRIVATE_KEY} provisioner.yaml -b"
- }
-
-}
