6becd86c378e7c3d059c4e7cc842f36390c7661d
prasanna489-ui <83752553+prasanna489-ui@users.noreply.github.com>
2021-05-24 18:52:24 +0530

    Create ansible.sh

---
+++ ansible.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+sudo apt install apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update -y
+sudo apt install ansible -y
+
+sudo cp /tmp/moodle.conf /etc/apache2/sites-available/
+sudo ansible-playbook /tmp/moodle.yaml
+sudo chown -R www-data:www-data /var/www/html/moodle/
+sudo chmod -R 755 /var/www/html/moodle/
+sudo chown www-data /var/www/html/moodledata
+
+sudo a2enmod rewrite
+sudo a2ensite moodle.conf
+sudo systemctl restart apache2
